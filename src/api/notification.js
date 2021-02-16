8ea2a57a2f4536acd74319651515401afc763722
chaijiawei <jiawei_chai@126.com>
2021-02-16 17:17:59 +0800

    标记消息为已读

--- src/api/notification.js
+++ src/api/notification.js
@@ -3,3 +3,13 @@ import { authRequest } from '@/utils/request'
 export function getNotificationStats() {
   return authRequest('notifications/stats')
 }
+
+export function getNotifications() {
+  return authRequest('notifications')
+}
+
+export function readNotifications() {
+  return authRequest('notifications/mark_read', {
+    method: 'put'
+  })
+}
